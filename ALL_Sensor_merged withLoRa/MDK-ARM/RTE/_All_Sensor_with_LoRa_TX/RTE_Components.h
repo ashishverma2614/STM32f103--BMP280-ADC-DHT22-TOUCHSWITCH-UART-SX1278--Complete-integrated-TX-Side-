3ac9f04fd47a3099fc8a385a83a915ee4d6d556c
ashish kumar verma <ashish2614@gmail.com>
2020-01-28 02:23:09 +0530

    All sensor data are send over integrated LoRa network

---
+++ ALL_Sensor_merged withLoRa/MDK-ARM/RTE/_All_Sensor_with_LoRa_TX/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ALL_Sensor_merged withLoRa' 
+ * Target:  'All_Sensor_with_LoRa_TX' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
